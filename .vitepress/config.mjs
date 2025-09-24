5371767bf54c669ee7b7208d0a4b5ffc01230938
Jerick120 <jerick120@Jericks-MacBook-Pro.local>
2025-09-24 09:30:58 +0530

    fix favicons

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,23 +7,23 @@ export default defineConfig({
   head: [
     ["link", { rel: "canonical", href: "https://bothunt.co" }],
 
-    ['link', { rel: 'icon', href: 'https://bothunt.co/logo.svg' }],
+    ['link', { rel: 'icon', href: 'https://bothunt.co/favicon-96x96.png' }],
 
     ["meta", { property: "og:title", content: "Bothunt Webhook API" }],
     ["meta", { property: "og:description", content: "Official guide for the Bothunt Webhook API." }],
     ["meta", { property: "og:type", content: "website" }],
     ["meta", { property: "og:url", content: "https://bothunt.co" }],
-    ["meta", { property: "og:image", content: "https://bothunt.co/logo.svg" }],
+    ["meta", { property: "og:image", content: "https://bothunt.co/favicon-96x96.png" }],
 
     ["meta", { name: "twitter:card", content: "summary_large_image" }],
     ["meta", { name: "twitter:title", content: "Bothunt Webhook API" }],
     ["meta", { name: "twitter:description", content: "Official guide for the Bothunt Webhook API." }],
-    ["meta", { name: "twitter:image", content: "https://bothunt.co/logo.svg" }],
+    ["meta", { name: "twitter:image", content: "https://bothunt.co/favicon-96x96.png" }],
   ],
   cleanUrls: true,
   themeConfig: {
     search: { provider: 'local' },
-    logo: "https://bothunt.co/logo.svg",
+    logo: "https://bothunt.co/favicon.svg",
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: 'https://bothunt.co' }
@@ -40,7 +40,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'discord', ariaLabel: 'Discord Server', link: 'https://bothunt.co/server' }
+      { icon: 'discord', ariaLabel: 'Discord Server', link: 'https://bothunt.co/join/333749099897683980?user_id=466489412885282816' }
     ]
   }
 })
